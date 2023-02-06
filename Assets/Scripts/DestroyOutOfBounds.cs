5bec5a585897fea30bdd05ae6789cd2253dd971a
Jasmin <122053640+jahernandez16@users.noreply.github.com>
2023-02-06 11:59:16 -0800

    updated outofbounds script and tested out the game

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (transform.position.z > topBound)
-       {
-            Destroy(gameObject);
-       }  else if (transform.position.z < lowerBound)
-       {
-            Destroy(gameObject);
-       }
+          // If and object goes past the player view in the game, remove that object
+          if (transform.position.z > topBound)
+          {
+               Destroy(gameObject);
+          }else if (transform.position.z < lowerBound)
+          {
+               Debug.Log("Game Over!");
+               Destroy(gameObject);
+          }
     }
 }
